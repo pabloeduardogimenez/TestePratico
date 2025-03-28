446c6c9ef1a8cf223c827ddf57a9cf5552ad0de9
Pablo Eduardo Gimenez <pabloedug7@gmail.com>
2025-03-28 14:40:10 -0300

    add

--- app/Policies/ControleUsuarioPolicy.php
+++ app/Policies/ControleUsuarioPolicy.php
@@ -11,9 +11,7 @@ class ControleUsuarioPolicy
     if ( $user->permissao_do_usuario == "Administrador" ) {
         return true;
     }
-    if ( $user->permissao_do_usuario == " " ) {
-        return true;
-    }
+    
 
     return false;
    }
